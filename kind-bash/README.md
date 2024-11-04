4b675212b2d6d8b9d0f1e40bf758938dae6a7933
fada <fada@mofada.cn>
2024-11-04 21:24:04 +0800

    1. init

---
+++ kind-bash/README.md
@@ -0,0 +1,21 @@
+# Eips 命令 - [Kindle屏幕控制](https://wiki.mobileread.com/wiki/Eips)
+
+## 1. Eips 命令简介
+
+Kindles有一个内置的eips命令，可以清除屏幕，可以打印未定义的屏幕，可以滚动未定义的屏幕，并且可以将PNG或JPG文件显示到屏幕上。
+
+## 2. Eips 命令语法
+
+### 2.1 清除屏幕
+
+```shell
+eips -c
+```
+
+- 清除屏幕会有残影，可以使用一张黑色的图片来清除屏幕
+
+### 2.2 显示图片
+
+```shell
+eips -g <图片路径>
+```
---
+++ kind-bash/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 获取电池电量
+batteryLevel=$(gasgauge-info -s)
+
+# 如果电池电量小于10，显示低电量图像
+if [ "$batteryLevel" -lt 10 ]; then
+  eips -g fill.png # 使用黑色图像填充屏幕
+  eips -c  # 清屏
+  eips -g low-battery.png  # 显示低电量图像
+  exit 0
+fi
+
+# 下载截图图像
+if ! curl -s -o screenshot.png http://118.25.193.183:3000/; then
+  echo "下载截图失败"
+  exit 1
+fi
+
+# 使用黑色图像填充屏幕
+eips -g fill.png
+
+# 清屏
+eips -c
+
+# 显示截图图像
+eips -g screenshot.png
