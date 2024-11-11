2137f2b61d9ffd5544d91595cea861583d12ab95
fada <fada@mofada.cn>
2024-11-11 16:35:42 +0800

    feat(weather): 实现天气数据接口并优化页面展示
    - 新增通用请求函数和天气相关 API- 更新组件以使用实时和未来天气数据
    - 优化日期和时间显示格式
    - 调整天气图标和信息的展示逻辑
    - 新增类型定义以规范数据结构
    - 配置代理以支持跨域请求

--- packages/kindle-dashboard/vite.config.ts
+++ packages/kindle-dashboard/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api/': {
+				changeOrigin: true,
+				target: 'https://devapi.qweather.com/v7',
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 })
