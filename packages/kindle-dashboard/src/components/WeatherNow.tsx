b4d1b70f435a47269dc09abdd614d8c8f3b8989e
fada <fada@mofada.cn>
2024-12-17 15:09:50 +0800

    style(kindle-dashboard): 优化 WeatherNow 组件的样式
    - 调整天气图标和温度显示的样式，提高可读性
    - 优化时间显示逻辑，处理 obsTime 可能为 undefined 的情况
    - 调整布局和间距，提升整体视觉效果

--- packages/kindle-dashboard/src/components/WeatherNow.tsx
+++ packages/kindle-dashboard/src/components/WeatherNow.tsx
@@ -15,8 +15,9 @@ export function WeatherNow({now}: { now: INow }) {
 		<header className="min-h-0 flex-[3] flex border-b-4 border-b-black"
 		        aria-label="当前天气信息">
 			{/* 左侧时间显示 */}
-			<div className="flex flex-col p-4 gap-2">
-				<i className={`qi-${now?.icon} flex-1 text-[160px]`} aria-label="天气图标"/>
+			<div className="flex flex-col p-4 gap-4 justify-end">
+				<i className={`qi-${now?.icon} text-[200px] leading-[200px]`}
+				   aria-label="天气图标"/>
 
 				{/* 当前时间 */}
 				<p className="text-xs flex-none">
@@ -29,7 +30,7 @@ export function WeatherNow({now}: { now: INow }) {
 				<h1 className="text-[42px]" aria-label="城市名称">{cityName}</h1>
 
 				{/* 温度显示 */}
-				<div className="text-[100px]">
+				<div className="text-[100px] leading-[100px]">
 					<span>{now?.temp}</span>
 					<span className="text-[50px]">℃</span>
 				</div>
